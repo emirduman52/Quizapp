4df5844fc604819e17f5dd7133c951a95e7735fa
Emir Duman <emirduman@mac.speedport.ip>
2025-10-24 17:39:21 +0200

    did some changes on the quiz card and desgin, added the jason array

---
+++ script.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+      "frage": "Was bedeutet der Begriff 'Tawhid' im islamischen Glauben?",
+      "antworten": [
+        { "text": "Die Einheit und Einzigkeit Gottes", "korrekt": true },
+        { "text": "Das Pflichtgebet der Muslime", "korrekt": false },
+        { "text": "Die Pilgerfahrt nach Mekka", "korrekt": false },
+        { "text": "Das islamische Gesetz (Scharia)", "korrekt": false }
+      ]
+    },
+    {
+      "frage": "Welche Stadt war die erste Qibla (Gebetsrichtung) der Muslime, bevor sie nach Mekka geändert wurde?",
+      "antworten": [
+        { "text": "Jerusalem (Al-Quds)", "korrekt": true },
+        { "text": "Medina", "korrekt": false },
+        { "text": "Kufa", "korrekt": false },
+        { "text": "Damaskus", "korrekt": false }
+      ]
+    },
+    {
+      "frage": "Wie heißt die Überlieferungssammlung, die die Aussprüche und Handlungen des Propheten Muhammad enthält?",
+      "antworten": [
+        { "text": "Hadith", "korrekt": true },
+        { "text": "Sunnah", "korrekt": false },
+        { "text": "Fiqh", "korrekt": false },
+        { "text": "Tafsir", "korrekt": false }
+      ]
+    },
+    {
+      "frage": "Welcher Engel überbrachte dem Propheten Muhammad die Offenbarung?",
+      "antworten": [
+        { "text": "Gabriel (Dschibril)", "korrekt": true },
+        { "text": "Michael (Mika'il)", "korrekt": false },
+        { "text": "Israfil", "korrekt": false },
+        { "text": "Azra'il", "korrekt": false }
+      ]
+    },
+    {
+      "frage": "Wie lautet der islamische Begriff für das Gemeinschaftsgebet am Freitag?",
+      "antworten": [
+        { "text": "Salat al-Jumuʿah", "korrekt": true },
+        { "text": "Salat al-Fajr", "korrekt": false },
+        { "text": "Salat al-Isha", "korrekt": false },
+        { "text": "Salat al-Maghrib", "korrekt": false }
+      ]
+    }
+  ];
+  
